8acea462a19300689db7afcd904690b8daf2b58d
LEVI YEO <levi.yeo@kcl.ac.uk>
2022-11-26 17:47:48 +0000

    transfers command
    added this command responsible for processing incoming transfers.
    
    It will add the amount to the user in the request

---
+++ lessons/management/commands/process_transfers.py
@@ -0,0 +1,25 @@
+'''A command for processing the incoming transfers of the app'''
+from django.core.management.base import BaseCommand
+
+from lessons.models import *
+
+
+class Command(BaseCommand):
+    '''A class responsible for processing all incoming transfers'''
+    
+    def handle(self, *args, **options):
+        print("Processing")
+        transfers = Transfer.objects.filter(state="I")
+        for transfer in transfers.iterator():
+            reference = transfer.reference
+            # refeference is in form stud_id-inv_id
+            stud_id = int(reference.split("-")[0])
+            user = User.objects.get(URN=stud_id)
+            student = Student.objects.get(user=user)
+            
+            balance_added = transfer.amount
+            student.balance += balance_added
+            transfer.state = "C"
+            transfer.save()
+            student.save()
+        print("Finished")
