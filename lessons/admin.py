c499fed57e998515063fe4f3d1045825e688175f
LEVI YEO <levi.yeo@kcl.ac.uk>
2022-11-26 04:00:11 +0000

    added to admin display

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from lessons.models import *
 
-# Register your models here.
+
+# Registered models.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['URN',
+                   'first_name',
+                   'last_name',
+                   'email',
+                   'role',
+                   'password']
+    
+    
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                   'availability',
+                   'balance']
+
+@admin.register(Admin)
+class AdminAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                   'school']
+    
+    
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                    'school',
+                   'availability']
+    
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ['schoolID',
+                    'name']
