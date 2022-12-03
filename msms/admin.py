e7ffc8318222de230d8310e49b1eebceb446f833
LEVI YEO <levi.yeo@kcl.ac.uk>
2022-12-03 01:20:33 +0000

    admin display is more descriptive

--- msms/admin.py
+++ msms/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
 from .models import User, Teacher, Student, Admin
 
-admin.site.register(User)
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Admin)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id",
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "is_staff",
+                    "is_student",
+                    "is_teacher",
+                    "is_admin",
+                    "password"]
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                   'availability',
+                   'balance']
+
+
+@admin.register(Admin)
+class AdminAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                   'school']
+    
+    
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                    'school',
+                   'availability']
