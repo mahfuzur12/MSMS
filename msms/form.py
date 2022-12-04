9432a8632bb6e1a59c829142af384469d4428c39
daddyyogmafs <K21038343@kcl.ac.uk>
2022-12-04 12:30:23 +0000

    remove registration for admin

--- msms/form.py
+++ msms/form.py
@@ -57,26 +57,3 @@ class TeacherSignUpForm(UserCreationForm):
         teacher.availability = self.cleaned_data.get('availability')
         return teacher
     
-class AdminSignUpForm(UserCreationForm):
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-    email = forms.EmailField(max_length=40)
-    
-    def clean_email(self):
-        if User.objects.filter(email=self.cleaned_data['email']).exists():
-            raise forms.ValidationError("A user with that email already exists.")
-        return self.cleaned_data['email']
-    
-    class Meta(UserCreationForm.Meta):
-        model = User
-    
-    @transaction.atomic
-    def save(self):
-        user:User = super().save(commit=False)
-        user.is_admin = True
-        user.first_name = self.cleaned_data.get('first_name')
-        user.last_name = self.cleaned_data.get('last_name')
-        user.email = self.cleaned_data.get('email')
-        user.save()
-        admin = Admin.objects.create(user=user)
-        return admin
