da6985395c2a9c464cdfd3b2c3976e2af1a67cc1
daddyyogmafs <K21038343@kcl.ac.uk>
2022-12-04 17:06:55 +0000

    reset and change password works

--- msms/form.py
+++ msms/form.py
@@ -60,6 +60,5 @@ class EditProfileForm(UserChangeForm):
         fields = (
             'email',
             'first_name',
-            'last_name',
-            'password'
+            'last_name'
         )
--- msms/urls.py
+++ msms/urls.py
@@ -17,7 +17,7 @@ from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
 from msms import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,5 +30,10 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('lesson/', include('lessons.urls')),
     path('profile/', views.view_profile, name='view_profile'),
-    path('profile/edit/', views.edit_profile, name='edit_profile')
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/password/', views.change_password, name='change_password'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name='reset_password'),
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), name='password_reset_confirm'),
+    path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), name='password_reset_complete')
 ]
