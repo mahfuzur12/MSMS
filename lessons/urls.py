2354571be27ee3bee602ae7160fe3f771508d320
LEVI YEO <levi.yeo@kcl.ac.uk>
2022-12-04 20:31:57 +0000

    Update urls.py

--- lessons/urls.py
+++ lessons/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('request/', views.RequestLesson.as_view(), name='request_lesson'),
     path('edit/<pk>', views.EditLesson.as_view(), name='edit_lesson'),
     path('view/', views.ViewLessons.as_view(), name='view_lessons'),
+    path('options/', views.options, name="lesson_options")
 ]
