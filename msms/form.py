daf3348577b0d4e6953f121856daa7dab0b2b9c6
daddyyogmafs <K21038343@kcl.ac.uk>
2022-12-04 14:07:39 +0000

    Revert "Merge branch 'profile' of https://github.kcl.ac.uk/k21009934/seg-sgp-project-2022 into profile"
    This reverts commit 2a1ab250bc57586ce0601283a4fbd41fa151dec1, reversing
    changes made to dfb9d6a1fc55a748810c3e9f9ee9e6432eec1b54.

--- msms/form.py
+++ msms/form.py
@@ -1,10 +1,9 @@
 from enum import unique
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.core.validators import RegexValidator
 from django.db import transaction
 from .models import Student, Teacher, Admin, User
-from django.contrib.auth.models import User
 
 class StudentSignUpForm(UserCreationForm):
     first_name = forms.CharField(required=True)
@@ -53,15 +52,27 @@ class TeacherSignUpForm(UserCreationForm):
         user.save()
         teacher = Teacher.objects.create(user=user)
         return teacher
-
-class EditProfileForm(UserChangeForm):
-
-    class Meta:
+    
+class AdminSignUpForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField(max_length=40)
+    
+    def clean_email(self):
+        if User.objects.filter(email=self.cleaned_data['email']).exists():
+            raise forms.ValidationError("A user with that email already exists.")
+        return self.cleaned_data['email']
+    
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = (
-            'email',
-            'first_name',
-            'last_name',
-            'password'
-        )
-
+    
+    @transaction.atomic
+    def save(self):
+        user:User = super().save(commit=False)
+        user.is_admin = True
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+        user.email = self.cleaned_data.get('email')
+        user.save()
+        admin = Admin.objects.create(user=user)
+        return admin
