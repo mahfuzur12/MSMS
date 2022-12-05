8189e834a22e2949d6b2b2384321e7f4096645e2
LEVI YEO <levi.yeo@kcl.ac.uk>
2022-12-05 05:26:10 +0000

    lesson cancel
    Lesson cancel auto removes related invoice if one exists

--- lessons/models.py
+++ lessons/models.py
@@ -28,7 +28,7 @@ class Lesson(models.Model):
     num_lessons = models.IntegerField(choices=[(num, num) for num in range(1, 11)], default=1)
     interval = models.IntegerField(choices=[(1, 1),(2, 2)], default=1)
     student = models.ForeignKey(Student, null=True, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE)
     # choices from 30mins, 45mins, 1hr
     duration = models.DurationField(choices=[(timedelta(minutes=mins), timedelta(minutes=mins)) for mins in [30, 45, 60]], default=30)
     state = models.CharField(max_length=20, choices=[("R","request"),("B","booking"),("C","cancelled")], default="R")
@@ -53,6 +53,18 @@ class Lesson(models.Model):
             self.state = "R"
             self.save()
             return None
+    
+    
+    def cancel(self):
+        if self.state == "C":
+            return
+        
+        invoice = Invoice.objects.filter(lesson=self)
+        if invoice.exists():
+            invoice.delete()
+        
+        self.state = "C"
+        self.save()
     
     
     def __str__(self):
