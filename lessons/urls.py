b354a07b58108ddac59c144bc6a6d54aef617ad9
LEVI YEO <levi.yeo@kcl.ac.uk>
2022-12-05 05:30:46 +0000

    Changes to request and edit
    Request and Edit pages now have their own versions for each type of user. Cancel and book views were also created which do what they say on the tin

--- lessons/urls.py
+++ lessons/urls.py
@@ -4,8 +4,17 @@ from lessons import views
 
 urlpatterns = [
     path('availability/', views.availability, name='availability'),
-    path('request/', views.RequestLesson.as_view(), name='request_lesson'),
-    path('edit/<pk>', views.EditLesson.as_view(), name='edit_lesson'),
+    path('request/', views.request_lesson, name='request_lesson'),
+    path('request/student/', views.StudentRequestLesson.as_view(), name='student_request_lesson'),
+    path('request/teacher/', views.TeacherRequestLesson.as_view(), name='teacher_request_lesson'),
+    path('request/admin/', views.AdminRequestLesson.as_view(), name='admin_request_lesson'),
+    path('edit/<pk>', views.edit_lesson, name='edit_lesson'),
+    path('edit/student/<pk>/', views.StudentEditLesson.as_view(), name='student_edit_lesson'),
+    path('edit/teacher/<pk>/', views.TeacherEditLesson.as_view(), name='teacher_edit_lesson'),
+    path('edit/admin/<pk>/', views.AdminEditLesson.as_view(), name='admin_edit_lesson'),
     path('view/', views.ViewLessons.as_view(), name='view_lessons'),
-    path('options/', views.options, name="lesson_options")
+    path('options/', views.options, name="lesson_options"),
+    path('cancel/<pk>/', views.cancel_lesson, name="cancel_lesson"),
+    path('book/<pk>/', views.book_lesson, name="book_lesson"),
+    path('finances/', views.finances, name="finances"),
 ]
