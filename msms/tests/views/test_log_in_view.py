a6784a218e4938fca945f22eb2451410095f4b6b
Rahman, Mahfuzur (k21038343) <mahfuzur.rahman@kcl.ac.uk>
2022-12-06 14:24:37 +0000

    more tests

--- msms/tests/views/test_log_in_view.py
+++ msms/tests/views/test_log_in_view.py
@@ -8,7 +8,7 @@ class LogInViewTestCase(TestCase, LogInTester):
     
     def setUp(self):
         self.url = reverse('login')
-        User.objects.create_user(
+        self.user = User.objects.create_user(
             is_student = True,
             is_teacher = False,
             is_admin = False,
@@ -17,6 +17,7 @@ class LogInViewTestCase(TestCase, LogInTester):
             email = 'johndoe@example.org',
             password1 = 'Password123',
             password2 = 'Password123',
+            is_active = True,
         )
     
     def test_log_in_url(self):
@@ -45,4 +46,14 @@ class LogInViewTestCase(TestCase, LogInTester):
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'feed.html')
         
-    
+    def test_log_in_by_inactive_user(self):
+        self.user.is_active = False
+        self.user.save()
+        form_input = {'email': 'johndoe@example.org', 'password': 'Password123'}
+        response = self.client.post(self.url, form_input, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, AuthenticationForm()))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self._is_logged_in())
